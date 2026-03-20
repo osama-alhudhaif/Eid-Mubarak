ec8c6ad76332453f215b4086ae6c1884e02d2c25
Osama Alhudhaif <osama.alhudhaif@gmail.com>
2026-03-20 05:48:57 +0300

    Successfully added 50+ languages for Eid 🌙

---
+++ eid.adb
@@ -0,0 +1,1 @@
+with_text_io; use text_io; procedure eid is begin put_line("عيد مبارك"); end eid;
---
+++ eid.zig
@@ -0,0 +1,1 @@
+const std = @import("std"); pub fn main() !void { std.debug.print("عيد مبارك\n", .{}); }
